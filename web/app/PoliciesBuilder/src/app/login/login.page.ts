f17dc310e7dd9f7caba7c89457606c63e20c99e0
George Dobrin <georgedobrin11@gmail.com>
2019-07-06 14:06:11 +0300

    finished login UI

--- web/app/PoliciesBuilder/src/app/login/login.page.ts
+++ web/app/PoliciesBuilder/src/app/login/login.page.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core'; 
+import { Slides } from 'ionic-angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
+
 export class LoginPage implements OnInit {
+
+  @ViewChild('slides') slides: Slides;
+  isLastPagePromise;
+  isLastPage = false;
 
   slideOpts = {
     initialSlide: 0,
@@ -17,6 +24,21 @@ export class LoginPage implements OnInit {
   ngOnInit() {
   }
 
+  onNextClicked() {
+    console.log('next clicked');
+    this.slides.slideNext();
 
+  }
+
+  onSwipe() {
+    this.isLastPagePromise = this.slides.isEnd();
+    this.isLastPagePromise.then(res => {
+      this.isLastPage = res;
+    })
+  }
+
+  onLoginClicked() {
+    console.log('login clicked')
+  }
 
 }
